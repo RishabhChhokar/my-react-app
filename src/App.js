1ae9a03575b48a665c080c8ed323e72713cbbab0
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-02-28 17:52:50 +0530

    Adding prop functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import ExpenseItems  from "./Components/ExpenseItems";
+import ExpenseItems from "./Components/ExpenseItems";
 
 function App() {
+  let expenses = [
+    {
+      date: new Date(2021, 4, 20),
+      expenseItem: "Life Insurance",
+      itemPrice: 240000,
+      locationOfExpediture: "Utility",
+    },
+    {
+      date: new Date(2021, 3, 24),
+      expenseItem: "KFC Bucket",
+      itemPrice: 700,
+      locationOfExpediture: "Food and beverages",
+    },
+  ];
   return (
     <div>
-      <ExpenseItems></ExpenseItems>
+      <ExpenseItems
+        date={expenses[0].date}
+        expenseItem={expenses[0].expenseItem}
+        itemPrice={expenses[0].itemPrice}
+        locationOfExpediture={expenses[0].locationOfExpediture}
+      ></ExpenseItems>
+      <ExpenseItems
+        date={expenses[1].date}
+        expenseItem={expenses[1].expenseItem}
+        itemPrice={expenses[1].itemPrice}
+        locationOfExpediture={expenses[1].locationOfExpediture}
+      ></ExpenseItems>
     </div>
   );
 }
