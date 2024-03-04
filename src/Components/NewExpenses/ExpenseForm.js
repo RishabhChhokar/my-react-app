caf7e55d4626ed778579439e5395729b0a6589c0
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-03-05 00:53:48 +0530

    Added very basic onSumbit event handler.

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -4,37 +4,69 @@ const ExpenseForm = () => {
   const [enteredAmount, expenseAmountChange] = useState("");
   const [enteredDate, expenseDateChange] = useState("");
   const [enteredLocation, expenseLocationChange] = useState("");
+
+  // const [userInput,setUserInput] = useState({
+  //   enteredTitle : '',
+  //   enteredAmount : '',
+  //   enteredDate : '',
+  //   enteredLocation : ''
+  // })
   const titleChangeHandler = (event) => {
+    // setUserInput((prevState)=>{
+    //   return {...prevState,enteredTitle:event.target.value};
+    // })
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle : event.target.value
+    // })
     expenseTitleChange(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount : event.target.value
+    // })
     expenseAmountChange(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value
+    // })
     expenseDateChange(event.target.value);
   };
 
   const locationChangeHandler = (event) => {
+    // setUserInput({
+    //   ...userInput,
+    //   enteredLocation : event.target.value
+    // })
     expenseLocationChange(event.target.value);
   };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    let newExpenseItem = { enteredTitle, enteredAmount, enteredDate, enteredLocation }
+    console.log(newExpenseItem);
+  };
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div>
-        <lable>Expense Title</lable>
+        <label>Expense Title</label>
         <input type="text" onChange={titleChangeHandler} />
       </div>
       <div>
-        <lable>Expense Amount</lable>
+        <label>Expense Amount</label>
         <input type="Number" onChange={amountChangeHandler} />
       </div>
       <div>
-        <lable>Expense Date</lable>
+        <label>Expense Date</label>
         <input type="date" onChange={dateChangeHandler} />
       </div>
       <div>
-        <lable>Expense Location</lable>
+        <label>Expense Location</label>
         <input type="text" onChange={locationChangeHandler} />
       </div>
       <div>
