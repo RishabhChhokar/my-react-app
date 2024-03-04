5988754862b289883a6764cf712faa766060ab2e
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-03-04 15:47:46 +0530

    Added separate use case for each component element.

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -1,25 +1,41 @@
+import React, { useState } from "react";
 const ExpenseForm = () => {
+  const [enteredTitle, expenseTitleChange] = useState("");
+  const [enteredAmount, expenseAmountChange] = useState("");
+  const [enteredDate, expenseDateChange] = useState("");
+  const [enteredLocation, expenseLocationChange] = useState("");
+  const titleChangeHandler = (event) => {
+    expenseTitleChange(event.target.value);
+  };
 
-    // const titleExpenseHandler = (event) => {
-    //     console.log(event.target.value)
-    // }
+  const amountChangeHandler = (event) => {
+    expenseAmountChange(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    expenseDateChange(event.target.value);
+  };
+
+  const locationChangeHandler = (event) => {
+    expenseLocationChange(event.target.value);
+  };
   return (
     <form>
       <div>
         <lable>Expense Title</lable>
-        <input type="text" onChange={(event)=>console.log(event.target.value)} />
+        <input type="text" onChange={titleChangeHandler} />
       </div>
       <div>
         <lable>Expense Amount</lable>
-        <input type="Number" />
+        <input type="Number" onChange={amountChangeHandler} />
       </div>
       <div>
         <lable>Expense Date</lable>
-        <input type="date" />
+        <input type="date" onChange={dateChangeHandler} />
       </div>
       <div>
         <lable>Expense Location</lable>
-        <input type="text" />
+        <input type="text" onChange={locationChangeHandler} />
       </div>
       <div>
         <button type="Submit">Add Expense</button>
